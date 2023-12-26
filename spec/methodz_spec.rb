0a3c0408a4c47a392cd9bf749a577579c68abbae
Ryan Kulp <ryanckulp@gmail.com>
2023-12-26 16:06:09 -0500

    tests

--- spec/methodz_spec.rb
+++ spec/methodz_spec.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.describe Methodz do
-  it "has a version number" do
+  before do
+    @obj = Object.new
+  end
+
+  it 'has a version number' do
     expect(Methodz::VERSION).not_to be nil
+  end
+
+  it 'should patch Object class' do
+    expect(@obj.methods).to include(:methodz)
+  end
+
+  it 'should accept a single, non-parameterized argument' do
+    expect(@obj.methodz('var').count).to be < @obj.methodz.count
+  end
+
+  it 'should return the same results for either query syntax' do
+    expect(@obj.methodz('var')).to eq @obj.methodz(q: 'var')
+  end
+
+  it 'should respond with private methods' do
+    expect(@obj.private_methods).to include(@obj.methodz(q: 'sys', type: 'private').sample)
+  end
+
+  it 'should respond with protected methods' do
+    expect(@obj.protected_methods).to eq(@obj.methodz(type: 'protected'))
   end
 end
